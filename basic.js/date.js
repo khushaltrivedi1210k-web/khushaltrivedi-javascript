df0e03a5c6add6b9c65ed9d008de341386678e5d
khushaltrivedi1210k-web <khushaltrivedi1210k@gmail.com>
2026-06-02 11:00:53 +0000

    date.js

---
+++ basic.js/date.js
@@ -0,0 +1,12 @@
+// Date
+
+let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toLocalestring());
+// console.log(mydate.toDateString());
+
+// create a date new
+
+let createdate = new Date(2026, 5, 24)
+console.log(createdate.toLocaleDateString());
