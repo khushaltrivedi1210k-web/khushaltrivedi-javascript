474c563b7cd482437a2245fd7bc78e9577b5fcd0
khushaltrivedi1210k-web <khushaltrivedi1210k@gmail.com>
2026-06-02 13:19:16 +0000

    array.js

---
+++ basic.js/array.js
@@ -0,0 +1,32 @@
+// Array
+
+const myArr = [1,2,3,4]
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+// Array method
+
+// myArr.push(5)
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(6)
+// myArr.shift()
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// console.log(myArr);
+
+console.log("a", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("b", myArr);
+
+const myn2 = myArr.splice(0,3)
+console.log("c", myArr);
+console.log(myn2);
