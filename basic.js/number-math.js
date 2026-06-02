5e00b1a724b3e9a575594c10df2022c5d6603bfd
khushaltrivedi1210k-web <khushaltrivedi1210k@gmail.com>
2026-06-02 10:02:16 +0000

    number-math.js

---
+++ basic.js/number-math.js
@@ -0,0 +1,24 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance. toString().length);
+// console.log(balance.toFixed(2));
+
+const othernumber = 2445.853
+// console.log(othernumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(3));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.7));
+// console.log(Math.min(3,4,2,5));
+// console.log(Math.max(1,3,6,2));
+
+console.log(Math.random());
+
