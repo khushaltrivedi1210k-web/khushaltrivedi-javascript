dfe72f8bb8ad09ca22aab334dd4495e209070c31
khushaltrivedi1210k-web <khushaltrivedi1210k@gmail.com>
2026-06-09 11:37:33 +0000

    function.js

--- basic.js/object.js
+++ basic.js/object.js
@@ -3,37 +3,59 @@
 
 // object literals
 
-const mySym = Symbol("key1")
+// const mySym = Symbol("key1")
 
 
-const JsUser = {
-    name: "khushal",
-    "full name": "khushal trivedi",
-    [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "trivedi@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
+// const JsUser = {
+//     name: "khushal",
+//     "full name": "khushal trivedi",
+//     [mySym]: "mykey1",
+//     age: 18,
+//     location: "Jaipur",
+//     email: "trivedi@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
 
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "khushal@gmail.com"
+// JsUser.email = "khushal@gmail.com"
 // Object.freeze(JsUser)
-JsUser.email = "khushal@ispark.com"
+// JsUser.email = "khushal@ispark.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-    console.log(`hello user welcome back, ${this.email}`);
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+//     console.log(`hello user welcome back, ${this.email}`);
+// }
+
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+// console.log(Object.keys(name));
+
+const value = {
+    a: 1,
+    b: 2,
+    c: "hello"
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// value.a = 1
+// const {a} = value
+// const {b} = value
+// const {c} = value
+
+const {a,b,c} = value;
+const d = [a,b,c];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
